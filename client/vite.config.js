a488b031c4923593ce7a9ccff947e98de23d0ff5
nani-31 <darkbutterflystar31@gmail.com>
2025-07-18 21:20:04 +0530

    added

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import glsl from "vite-plugin-glsl";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: "/portfolio/",
+  plugins: [react(), tailwindcss(), glsl()],
+  server: {
+    // port: 8080,
+    host: true, // Ensure it listens on all interfaces
+  },
+});
