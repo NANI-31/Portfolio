f8058031fd0b212ba2e064436e9dd24521233cd7
nani-31 <darkbutterflystar31@gmail.com>
2025-07-19 08:37:24 +0530

    added all

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,34 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')).render(
+import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App.jsx";
+import GlobalProvider from "./context/GlobalProvider";
+import ProjectsPage from "./apps/projects/ProjectPage";
+import ProjectsPage1 from "./apps/projects/ProjectPage1";
+import Layout from "./components/Layout";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+// import Landing1 from './Landing1.jsx';
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <GlobalProvider>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/portfolio" element={<Layout />}>
+              <Route index element={<App />} />
+              <Route path="/portfolio/projects" element={<ProjectsPage />} />
+              <Route
+                path="/portfolio/projects/:id"
+                element={<ProjectsPage1 />}
+              />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
+    </GlobalProvider>
+  </StrictMode>
+);
